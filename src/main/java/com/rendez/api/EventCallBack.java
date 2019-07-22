f9bfc982536ac452560bb6af432e1852e6d6e026
liuwenjie <liuwenjie@zhongan.io>
2019-07-22 16:34:59 +0800

    updata to 1.2.7

--- src/main/java/com/rendez/api/EventCallBack.java
+++ src/main/java/com/rendez/api/EventCallBack.java
@@ -41,10 +41,10 @@ public abstract class EventCallBack {
         if(receipt.getLogInfoList() != null && receipt.getLogInfoList().size()>0){
             receipt.getLogInfoList().forEach(log -> {
                 List<Type> decodeResult = FunctionReturnDecoder.decode(Hex.toHexString(log.getData()), event.getNonIndexedParameters());
-                handleEvent(decodeResult,receipt.getTxHash());
+                handleEvent(decodeResult,receipt.getTxHash(),receipt.getHeight().longValue());
             });
         }else{
-            handleEvent(Arrays.asList(),receipt.getTxHash());
+            handleEvent(Arrays.asList(),receipt.getTxHash(),receipt.getHeight().longValue());
         }
     }
 
@@ -52,5 +52,5 @@ public abstract class EventCallBack {
      * 具体处理业务逻辑代码
      * @param decodeResult
      */
-    public abstract void handleEvent(List<Type> decodeResult,String txHash);
+    public abstract void handleEvent(List<Type> decodeResult,String txHash,Long height);
 }
--- src/main/java/com/rendez/api/TransactionReceipt.java
+++ src/main/java/com/rendez/api/TransactionReceipt.java
@@ -110,20 +110,23 @@ public class TransactionReceipt {
         RLPItem toRLP = (RLPItem) receipt.get(3);
         to = com.rendez.api.util.ByteUtil.bytesToHex(toRLP.getRLPData());
 
-        RLPItem postTxStateRLP = (RLPItem) receipt.get(4);
+        // return data
+        RLPItem returnRLP = (RLPItem) receipt.get(4);
 
-        RLPItem cumulativeGasRLP = (RLPItem) receipt.get(5);
+        RLPItem postTxStateRLP = (RLPItem) receipt.get(5);
 
-        RLPItem bloomRLP = (RLPItem) receipt.get(6);
-        RLPItem txHashRLP = (RLPItem) receipt.get(7);
+        RLPItem cumulativeGasRLP = (RLPItem) receipt.get(6);
+
+        RLPItem bloomRLP = (RLPItem) receipt.get(7);
+        RLPItem txHashRLP = (RLPItem) receipt.get(8);
         txHash = com.rendez.api.util.ByteUtil.bytesToHex(txHashRLP.getRLPData());
-        RLPItem contractAddressRLP = (RLPItem) receipt.get(8);
+        RLPItem contractAddressRLP = (RLPItem) receipt.get(9);
         contractAddress = com.rendez.api.util.ByteUtil.bytesToHex(contractAddressRLP.getRLPData());
 
-        RLPList logs = (RLPList) receipt.get(9);
-        RLPItem gasUsedRLP = (RLPItem) receipt.get(10);
+        RLPList logs = (RLPList) receipt.get(10);
+        RLPItem gasUsedRLP = (RLPItem) receipt.get(11);
 
-        RLPItem statusRLP = (RLPItem) receipt.get(11);
+        RLPItem statusRLP = (RLPItem) receipt.get(12);
 
         postTxState = nullToEmpty(postTxStateRLP.getRLPData());
         cumulativeGas = cumulativeGasRLP.getRLPData();
